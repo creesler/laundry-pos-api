feef5776c1b619890e8e86ee4a7face5b109cf86
creesler <creesler@gmail.com>
2025-06-17 00:45:25 +0800

    \Add service worker registration to PWARoot component"

--- src/app/components/PWARoot.tsx
+++ src/app/components/PWARoot.tsx
@@ -1,15 +1,21 @@
 'use client';
 
+import { useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import createEmotionCache from '../utils/createEmotionCache';
+import { registerServiceWorker } from '../pwa';
 import theme from '../theme';
 import PWAInstallButton from './PWAInstallButton';
 
 const clientSideEmotionCache = createEmotionCache();
 
 export default function PWARoot({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    registerServiceWorker();
+  }, []);
+
   return (
     <CacheProvider value={clientSideEmotionCache}>
       <ThemeProvider theme={theme}>
