35ac30faaa63c1ceef3443f54058fd31d7d3588b
creesler <creesler@gmail.com>
2025-06-17 00:17:01 +0800

    \Update MUI Next.js integration and add SWC dependencies"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import PWARoot from './components/PWARoot';
 
 const inter = Inter({ subsets: ['latin'] });
