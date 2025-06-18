b64bb6db7196a210787c612180e97e2bf20546e9
creesler <creesler@gmail.com>
2025-06-19 05:09:28 +0800

    refactor: change 'Save to Server' button label to 'Sync' for better UI conciseness

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -749,8 +749,8 @@ export default function Header({
                 <Button
                   color="inherit"
                   onClick={syncWithServer}
-                  startIcon={<CloudUploadIcon />}
                   disabled={!hasUnsavedChanges || isSaving}
+                  startIcon={<CloudUploadIcon />}
                 >
                   {isSaving ? 'Syncing...' : 'Sync'}
                 </Button>
