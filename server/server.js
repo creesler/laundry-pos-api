34b8fee630e196d6dfd24fa624ddcf956abdc522
creesler <60352120+creesler@users.noreply.github.com>
2025-08-15 23:53:25 +0800

    Fix: Set most permissive CORS configuration

--- server/server.js
+++ server/server.js
@@ -29,8 +29,8 @@ connectDB();
 // CORS middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
   
   // Handle preflight
